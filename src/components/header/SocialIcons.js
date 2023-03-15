747f1c1258e0e1b2d4974d2f7d63c9c4dc667887
Marco Di Summa <mdisumma@yahoo.com>
2023-03-15 10:33:08 +0000

    min

--- src/components/header/SocialIcons.js
+++ src/components/header/SocialIcons.js
@@ -5,48 +5,48 @@ import { BsEnvelope } from "react-icons/bs";
 import { FaFileDownload } from "react-icons/fa";
 
 function SocialIcons() {
-	return (
-		<nav className="socialIcons">
-			<ul>
-				<li>
-					<a
-						href="https://www.linkedin.com/in/marco-di-summa-1a5146182/"
-						target="blanck"
-						rel="noreferrer"
-					>
-						<RiLinkedinLine />
-					</a>
-				</li>
-				<li>
-					<a
-						href="https://twitter.com/MarcoDiSumma1"
-						target="blanck"
-						rel="noreferrer"
-					>
-						<RiTwitterLine />
-					</a>
-				</li>
-				<li>
-					<a
-						href="https://github.com/mdisumma"
-						target="blanck"
-						rel="noreferrer"
-					>
-						<RiGithubLine />
-					</a>
-				</li>
-				<li>
-					<a href="./asset/cv/Marco_Di_Summa_Resume.pdf" download>
-						<FaFileDownload />
-					</a>
-				</li>
-				<li>
-					<a href="mailto:mdisumma@yahoo.com">
-						<BsEnvelope />
-					</a>
-				</li>
-			</ul>
-		</nav>
-	);
+  return (
+    <nav className="socialIcons">
+      <ul>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/marco-di-summa-1a5146182/"
+            target="blanck"
+            rel="noreferrer"
+          >
+            <RiLinkedinLine />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://twitter.com/MarcoDiSumma1"
+            target="blanck"
+            rel="noreferrer"
+          >
+            <RiTwitterLine />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/mdisumma"
+            target="blanck"
+            rel="noreferrer"
+          >
+            <RiGithubLine />
+          </a>
+        </li>
+        <li>
+          <a href="./asset/cv/MarcoDiSumma-Resume.pdf" download>
+            <FaFileDownload />
+          </a>
+        </li>
+        <li>
+          <a href="mailto:mdisumma@yahoo.com">
+            <BsEnvelope />
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 export default SocialIcons;
