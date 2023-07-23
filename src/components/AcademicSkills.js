8e5747f26979b20d6b8dac1a63eefb3f87ea0048
Marco Di Summa <mdisumma@yahoo.com>
2023-07-23 19:07:48 +0100

    acc skills

--- src/components/AcademicSkills.js
+++ src/components/AcademicSkills.js
@@ -1,33 +1,32 @@
 import "./AcademicSkills.css";
 
 function AcademinSkills() {
-	return (
-		<section id="accademicSkills">
-			<h2>Academic Skills</h2>
-			<div className="accademicSkills">
-				<ul>
-					<li>Web Essential</li>
-					<li>Interactive Design</li>
-					<li>Front-end Development</li>
-					<li>Back-end development</li>
-					<li>Database</li>
-					<li>REST Api</li>
-					<li>SEO</li>
-					<li>Analytics</li>
-				</ul>
-				<ul>
-					<li>Brand Principles</li>
-					<li>Logo Creation</li>
-					<li>Infographic</li>
-					<li>Layout Design</li>
-					<li>Media Production</li>
-					<li>Image Manipulation</li>
-					<li>Vector Graphic</li>
-					<li>Animation</li>
-				</ul>
-			</div>
-		</section>
-	);
+  return (
+    <section id="accademicSkills">
+      <h2>Academic Skills</h2>
+      <div className="accademicSkills">
+        <ul>
+          <li>Responsive Design</li>
+          <li>Front-end Development</li>
+          <li>Back-end development</li>
+          <li>Database</li>
+          <li>REST Api</li>
+          <li>SEO</li>
+          <li>Analytics</li>
+        </ul>
+        <ul>
+          <li>Brand Principles</li>
+          <li>Logo Creation</li>
+          <li>Layout Design</li>
+          <li>Media Production</li>
+          <li>Vector Graphic</li>
+          <li>Animation</li>
+          <li>User research</li>
+          <li>UX & UI</li>
+        </ul>
+      </div>
+    </section>
+  );
 }
 
 export default AcademinSkills;
--- src/components/skills/SoftwareSkills.js
+++ src/components/skills/SoftwareSkills.js
@@ -12,56 +12,56 @@ import { IconContext } from "react-icons";
 import "./Skills.css";
 
 function SoftwareSkills() {
-	return (
-		<section id="skills">
-			<h2>Software skills</h2>
-			<IconContext.Provider
-				value={{
-					color: "var(--mid-tone)",
-					size: 35,
-				}}
-			>
-				<ul>
-					<li>
-						<SiAdobe />
-						Adobe
-					</li>
-					<li>
-						<SiAdobeillustrator />
-						Illustrator
-					</li>
-					<li>
-						<SiAdobephotoshop />
-						Photoshop
-					</li>
-					<li>
-						<SiAdobeindesign />
-						InDesign
-					</li>
-					<li>
-						<SiAdobexd />
-						Xd
-					</li>
-					<li>
-						<TbVectorBezier />
-						SVG
-					</li>
-					<li>
-						<SiVisualstudiocode />
-						Visual Studio
-					</li>
-					<li>
-						<SiPostman />
-						Postman
-					</li>
-					<li>
-						<SiNetlify />
-						Netlify
-					</li>
-				</ul>
-			</IconContext.Provider>
-		</section>
-	);
+  return (
+    <section id="skills">
+      <h2>Software skills</h2>
+      <IconContext.Provider
+        value={{
+          color: "var(--mid-tone)",
+          size: 35,
+        }}
+      >
+        <ul>
+          <li>
+            <SiAdobe />
+            Adobe
+          </li>
+          <li>
+            <SiAdobeillustrator />
+            Illustrator
+          </li>
+          <li>
+            <SiAdobephotoshop />
+            Photoshop
+          </li>
+          <li>
+            <SiFigma />
+            Figma
+          </li>
+          <li>
+            <SiAdobexd />
+            Xd
+          </li>
+          <li>
+            <TbVectorBezier />
+            SVG
+          </li>
+          <li>
+            <SiVisualstudiocode />
+            Visual Studio
+          </li>
+          <li>
+            <SiPostman />
+            Postman
+          </li>
+          <li>
+            <SiNetlify />
+            Netlify
+          </li>
+        </ul>
+      </IconContext.Provider>
+    </section>
+  );
 }
 
 export default SoftwareSkills;
