df8fd516f31ed39e82ed53168f4eee0a7a95dc56
Marco Di Summa <mdisumma@yahoo.com>
2023-07-23 19:16:39 +0100

    min

--- src/components/AcademicSkills.js
+++ src/components/AcademicSkills.js
@@ -7,22 +7,22 @@ function AcademinSkills() {
       <div className="accademicSkills">
         <ul>
           <li>Responsive Design</li>
+          <li>User research</li>
+          <li>UX & UI</li>
           <li>Front-end Development</li>
           <li>Back-end development</li>
           <li>Database</li>
           <li>REST Api</li>
           <li>SEO</li>
-          <li>Analytics</li>
         </ul>
         <ul>
+          <li>Analytics</li>
           <li>Brand Principles</li>
           <li>Logo Creation</li>
           <li>Layout Design</li>
           <li>Media Production</li>
           <li>Vector Graphic</li>
           <li>Animation</li>
-          <li>User research</li>
-          <li>UX & UI</li>
         </ul>
       </div>
     </section>
