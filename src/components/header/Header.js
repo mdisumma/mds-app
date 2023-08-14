2241f67b5fd9259e78b6bca274c5c5e593a5201a
Marco Di Summa <mdisumma@yahoo.com>
2023-08-14 09:34:35 +0100

    min

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,22 +4,22 @@ import SocialIcons from "./SocialIcons";
 import "./Header.css";
 
 function Header() {
-	return (
-		<>
-			<header>
-				<div className="banner">
-					<div className="logo">
-						<img src="./mds_logo.svg" alt="marco di summa logo" />
-					</div>
-					<div className="title">
-						<h1>Marco Di Summa</h1>
-						<p>Web Developer</p>
-					</div>
-				</div>
-				<SocialIcons />
-			</header>
-		</>
-	);
+  return (
+    <>
+      <header>
+        <div className="banner">
+          <div className="logo">
+            <img src="./mds_logo.svg" alt="marco di summa logo" />
+          </div>
+          <div className="title">
+            <h1>Marco Di Summa</h1>
+            <p>Web Designer and Developer</p>
+          </div>
+        </div>
+        <SocialIcons />
+      </header>
+    </>
+  );
 }
 
 export default Header;
